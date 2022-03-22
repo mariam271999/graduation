795ec5abe2e7a2e8607179ef036a8f2b078e319d
mariam <mariamyumyum604@gmail.com>
2022-03-22 06:27:00 -0700

    https://github.com/mariam271999/graduation

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,14 +3,22 @@ const userController = require('../controllers/user.controller')
 const validation = require('../validator/register.validation')
 
 module.exports= (app)=>{
-
+//get user
     app.get('./api/user',
     userController.all)
-
+//add user
     app.post('/api/user/register',validation
     ,userController.creat)
-
+//login
     app.post('/api/user/login',userController.login)
-
+//update user
+app.put('/api/user/:id',
+userController.edit);
+//delete user
+app.delete('/api/uder/:id',
+userController.delete);
+//find user by id
+app.get('/api/user/:id',
+userController.findById)
 } 
 
